0bbc0db708c80272f1a7f9a35a45e49874fc1cfd
[Rocky Shorf] <[rockyshorf835@gmail.com]>
2025-06-20 18:48:20 +0530

    updated css, button, page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello World</p>
+    <div className="p-4">
+      <div className="p-4">
+      <Button variant="elevated">Hello world</Button>
+      </div>
+      <div>
+      <Input placeholder="This is an input"></Input>
     </div>
+    </div>
+    
   );
 }
